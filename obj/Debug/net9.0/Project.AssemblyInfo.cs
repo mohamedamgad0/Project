2d1fc5a920f818c91ffe8fac015a5a4082da6dc2
Mohamed Amgad <111206416+mohamedamgad0@users.noreply.github.com>
2024-12-04 23:05:30 +0200

    Version2

--- obj/Debug/net9.0/Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d0ab03cf220e25ac8ca2d1c7f8f6747bf74a41d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
